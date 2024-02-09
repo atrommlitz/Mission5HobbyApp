87434aea158af1e864ebdf6c38bf7df4b3eed3ed
adamtrommlitz <adamtrommlitz3@gmail.com>
2024-02-08 18:39:50 -0700

    new comments

--- Mission5HobbyApp/wwwroot/js/site.js
+++ Mission5HobbyApp/wwwroot/js/site.js
@@ -1,12 +1,14 @@
 ﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+// Write your JavaScript code
+//Grab the total pay button and make an on click event handler 
 $('#totalPayButton').on("click", function () {
+    //Make a training hours variable to grab the input from the user and parse it as a float
     var trainingHours = parseFloat($('#trainingHours').val());
 
     if (!isNaN(trainingHours)) {
-        // Perform the calculation
+        // Perform the calculation with 400 dollars per hour
         var totalPay = trainingHours * 400;
 
         // Update the content of the HTML element with the calculated total
